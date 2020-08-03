c713410c41c0d80a846ef039431e6f29dbd424e0
iamm3chanic <64973921+iamm3chanic@users.noreply.github.com>
2020-08-03 17:03:13 +0300

    Add files via upload

---
+++ task1/src/test/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  //  import logic.Browser;
+  public static void main(String[] args) {
+      System.out.println("AVIACOMPANY IAMM3CHANIC");
+      Browser consultant = new Browser();
+      consultant.browse();
+      /* Other variant of seller */
+      // AbstractSeller agent = new Agent();
+      // agent.offer();
+  }
+}
