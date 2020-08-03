3fc8827b114cc8828cd220d62b4c279ae59adb59
iamm3chanic <64973921+iamm3chanic@users.noreply.github.com>
2020-08-03 17:51:42 +0300

    Update Main.java

--- task1/src/test/Main.java
+++ task1/src/test/Main.java
@@ -1,11 +1,8 @@
 public class Main {
   //  import logic.Browser;
   public static void main(String[] args) {
-      System.out.println("AVIACOMPANY IAMM3CHANIC");
-      Browser consultant = new Browser();
-      consultant.browse();
-      /* Other variant of seller */
-      // AbstractSeller agent = new Agent();
-      // agent.offer();
+      Aviacompany aviacompany = new Aviacompany();
+      aviacompany.print();
   }
 }
+
